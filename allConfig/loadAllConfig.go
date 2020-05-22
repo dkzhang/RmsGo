7726cfddfd93a69398f66b80372ff04d0b6ffc1d
dkzhang@Office.PC <527656264@qq.com>
2020-05-22 15:33:21 +0800

    modify load config.

--- allConfig/loadAllConfig.go
+++ allConfig/loadAllConfig.go
@@ -3,14 +3,16 @@ package allConfig
 import (
 	"fmt"
 	"github.com/dkzhang/RmsGo/datebaseCommon/config"
+	"log"
 	"os"
 )
 
 func LoadAllConfig() (err error) {
 
-	err = config.LoadDbConfig(os.Getenv("dbconf"))
+	theDbConfig, err := config.LoadDbConfig(os.Getenv("dbconf"))
 	if err != nil {
 		return fmt.Errorf("config.LoadDbConfig error: %v", err)
 	}
+	log.Printf("%v", theDbConfig)
 	return nil
 }
