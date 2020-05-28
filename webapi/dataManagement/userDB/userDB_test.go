2ab6683ab1f1269026f305140d17362cf42129c5
dkzhang@Office.PC <527656264@qq.com>
2020-05-28 15:33:43 +0800

    update userDB ginkgo test.

--- webapi/dataManagement/userDB/userDB_test.go
+++ webapi/dataManagement/userDB/userDB_test.go
@@ -1,92 +1,82 @@
 package userDB_test
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/dkzhang/RmsGo/webapi/model/user"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"io/ioutil"
 )
+
+type TestDataFile struct {
+	Users []user.UserInfo `json:"users"`
+}
 
 var _ = Describe("UserDB", func() {
 
 	var (
-		user1 user.UserInfo
-		user2 user.UserInfo
-		user3 user.UserInfo
-		user4 user.UserInfo
+		testData = TestDataFile{}
 	)
 
 	BeforeEach(func() {
-		user1 = user.UserInfo{
-			UserName:       "zhang001",
-			ChineseName:    "张一一",
-			Department:     "部门一",
-			DepartmentCode: "DEP1",
-			Section:        "某科室一",
-			Mobile:         "12300001111",
-			Role:           user.RoleProjectChief,
-			Status:         user.StatusNormal,
-			Remarks:        "user1",
-		}
-		user2 = user.UserInfo{
-			UserName:       "zhang001",
-			ChineseName:    "张二二",
-			Department:     "部门一",
-			DepartmentCode: "DEP1",
-			Section:        "某科室一",
-			Mobile:         "12300002222",
-			Role:           user.RoleProjectChief,
-			Status:         user.StatusNormal,
-			Remarks:        "user2",
-		}
-		user3 = user.UserInfo{
-			UserName:       "zhang003",
-			ChineseName:    "张三三",
-			Department:     "部门一",
-			DepartmentCode: "DEP1",
-			Section:        "某科室三",
-			Mobile:         "12300003333",
-			Role:           user.RoleApprover,
-			Status:         user.StatusNormal,
-			Remarks:        "user3",
-		}
-		user4 = user.UserInfo{
-			UserName:       "zhang004",
-			ChineseName:    "张四四",
-			Department:     "部门四",
-			DepartmentCode: "DEP4",
-			Section:        "某科室四",
-			Mobile:         "12300004444",
-			Role:           user.RoleApprover,
-			Status:         user.StatusNormal,
-			Remarks:        "user4",
-		}
+		filedata, err := ioutil.ReadFile("userDB_test.json")
+		Expect(err).ShouldNot(HaveOccurred())
+
+		err = json.Unmarshal(filedata, &testData)
+		Expect(err).ShouldNot(HaveOccurred())
+
+		By(fmt.Sprintf("users = %v", testData.Users))
 	})
 
 	Describe("insert new user", func() {
-		Context("insert first user", func() {
+		Context("insert user with no name duplicate", func() {
 			It("should success", func() {
-				err := udb.InsertUser(user1)
+				err := udb.InsertUser(testData.Users[0])
 				Expect(err).ShouldNot(HaveOccurred(), "insert first user error", err)
 			})
+			It("should success", func() {
+				err := udb.InsertUser(testData.Users[1])
+				Expect(err).ShouldNot(HaveOccurred())
+			})
+			It("should success", func() {
+				err := udb.InsertUser(testData.Users[2])
+				Expect(err).ShouldNot(HaveOccurred())
+			})
 		})
-		Context("insert 2nd user", func() {
-			It("insert 2nd user with same name of first user should error", func() {
-				err := udb.InsertUser(user2)
+		Context("insert user with name duplicate", func() {
+			It("insert user with same name should error", func() {
+				testData.Users[3].UserName = testData.Users[0].UserName
+				err := udb.InsertUser(testData.Users[3])
 				Expect(err).Should(HaveOccurred())
 				By(fmt.Sprintf("error = %v", err))
 			})
 		})
-		Context("insert 3rd user", func() {
+
+		//Context("xxx", func() {
+		//	It("should success", func() {
+		//		td := TestDataFile{Users: []user.UserInfo{user1,user2, user3, user4}}
+		//		jsonStr, err := json.MarshalIndent(td, "", "    ")
+		//		Expect(err).ShouldNot(HaveOccurred())
+		//		err = ioutil.WriteFile("userDB_test.json", jsonStr, 0644)
+		//		Expect(err).ShouldNot(HaveOccurred())
+		//	})
+		//})
+	})
+
+	Describe("query user by name", func() {
+		Context("query user exist", func() {
 			It("should success", func() {
-				err := udb.InsertUser(user3)
-				Expect(err).ShouldNot(HaveOccurred())
+				user, err := udb.QueryUserByName(testData.Users[0].UserName)
+				Expect(err).ShouldNot(HaveOccurred(), "insert first user error", err)
+				Expect(user.Mobile).Should(Equal(testData.Users[0].Mobile))
 			})
 		})
-		Context("insert 4th user", func() {
-			It("should success", func() {
-				err := udb.InsertUser(user4)
-				Expect(err).ShouldNot(HaveOccurred())
+		Context("query user not exist", func() {
+			It("should err", func() {
+				_, err := udb.QueryUserByName(testData.Users[3].UserName)
+				Expect(err).Should(HaveOccurred(), "insert first user error", err)
+				By(fmt.Sprintf("error = %v", err))
 			})
 		})
 	})
