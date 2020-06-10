725bfab2d36cb547ac43887d40c2b58b35eb6626
dkzhang@Office.PC <527656264@qq.com>
2020-06-10 10:00:34 +0800

    accomplish gin applyLogin.

--- webapi/dataManagement/userTempDM/redisAndJwt.go
+++ webapi/dataManagement/userTempDM/redisAndJwt.go
@@ -6,9 +6,8 @@ import (
 	"github.com/dkzhang/RmsGo/datebaseCommon/redisOps"
 	"github.com/dkzhang/RmsGo/myUtils/encryptPassword"
 	"github.com/dkzhang/RmsGo/myUtils/genPasswd"
-	"github.com/dkzhang/RmsGo/webapi"
 	"github.com/dkzhang/RmsGo/webapi/dataManagement/userTempDM/config"
-	"log"
+	"github.com/dkzhang/RmsGo/webapi/dataManagement/userTempDM/security"
 	"net/http"
 	"strconv"
 	"strings"
@@ -16,14 +15,16 @@ import (
 )
 
 type RedisAndJwt struct {
-	TheRedis       *redisOps.Redis
-	TheLoginConfig config.LoginConfig
+	TheRedis         *redisOps.Redis
+	TheLoginConfig   config.LoginConfig
+	TheLoginSecurity security.LoginSecurity
 }
 
-func NewRedisAndJwt(redis *redisOps.Redis, cfg config.LoginConfig) RedisAndJwt {
+func NewRedisAndJwt(redis *redisOps.Redis, cfg config.LoginConfig, se security.LoginSecurity) RedisAndJwt {
 	return RedisAndJwt{
-		TheRedis:       redis,
-		TheLoginConfig: cfg,
+		TheRedis:         redis,
+		TheLoginConfig:   cfg,
+		TheLoginSecurity: se,
 	}
 }
 
@@ -79,7 +80,7 @@ func (rj RedisAndJwt) CreateToken(userID int) (token string, err error) {
 	atClaims["user_id"] = userID
 	atClaims["exp"] = time.Now().Add(rj.TheLoginConfig.TheTokenConfig.Expire).Unix()
 	at := jwt.NewWithClaims(jwt.SigningMethodHS256, atClaims)
-	token, err = at.SignedString([]byte(webapi.TheInfras.TheLoginSecurity.TheTokenSecurity.Key))
+	token, err = at.SignedString([]byte(rj.TheLoginSecurity.TheTokenSecurity.Key))
 	if err != nil {
 		return "", err
 	}
@@ -93,7 +94,7 @@ func (rj RedisAndJwt) CreateToken(userID int) (token string, err error) {
 }
 
 func (rj RedisAndJwt) ValidateToken(r *http.Request) (userID int, err error) {
-	userID, err = extractTokenMetadata(r)
+	userID, err = rj.extractTokenMetadata(extractToken(r))
 	if err != nil {
 		return -1, fmt.Errorf("extractTokenMetadata error: %v", err)
 	}
@@ -124,26 +125,16 @@ func extractToken(r *http.Request) string {
 	return ""
 }
 
-func verifyToken(r *http.Request) (*jwt.Token, error) {
-	tokenString := extractToken(r)
+func (rj RedisAndJwt) extractTokenMetadata(tokenString string) (userID int, err error) {
 	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
 		//Make sure that the token method conform to "SigningMethodHMAC"
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
 		}
-		return []byte(webapi.TheInfras.TheLoginSecurity.TheTokenSecurity.Key), nil
+		return []byte(rj.TheLoginSecurity.TheTokenSecurity.Key), nil
 	})
 	if err != nil {
-		log.Printf("jwt.Parse token error: %v", err)
-		return nil, err
-	}
-	return token, nil
-}
-
-func extractTokenMetadata(r *http.Request) (userID int, err error) {
-	token, err := verifyToken(r)
-	if err != nil {
-		return -1, fmt.Errorf("VerifyToken error: %v", err)
+		return -1, fmt.Errorf("VerifyToken jwt.Parse token error: %v", err)
 	}
 
 	claims, ok := token.Claims.(jwt.MapClaims)
