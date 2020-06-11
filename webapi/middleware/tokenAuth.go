8875cdfc675a27695e6a7f8b9143dfcfd2826bb5
dkzhang@Office.PC <527656264@qq.com>
2020-06-11 10:50:42 +0800

    accomplish /AllUsers .

--- webapi/middleware/tokenAuth.go
+++ webapi/middleware/tokenAuth.go
@@ -1,8 +1,10 @@
 package middleware
 
 import (
+	"github.com/dkzhang/RmsGo/myUtils/logMap"
 	"github.com/dkzhang/RmsGo/webapi"
 	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
 	"net/http"
 )
 
@@ -12,7 +14,12 @@ func TokenAuth() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		userID, err := webapi.TheInfras.TheUserTempDM.ValidateToken(c.Request)
 		if err != nil {
-			c.JSON(http.StatusUnauthorized, err.Error())
+			logMap.Log(logMap.NORMAL).WithFields(logrus.Fields{
+				"error": err,
+			}).Error("get userID from gin.Context failed.")
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"msg": "token验证未通过",
+			})
 			c.Abort()
 			return
 		}
