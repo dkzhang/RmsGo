38b5961b42c0a15b02ba80967762c0ecfb1e3f5a
dkzhang@Office.PC <527656264@qq.com>
2020-06-11 16:53:49 +0800

    accomplish part of UserAuthorityCheck .

--- webapi/authorityManagement/userCRUD.go
+++ webapi/authorityManagement/userCRUD.go
@@ -39,6 +39,34 @@ func init() {
 		Permission:  true,
 		Description: "Allow RoleController to do CRUD ops to all users",
 	})
+
+	TheUserAuthorityTable = append(TheUserAuthorityTable, UserAuthority{
+		RelationShipBetween: func(userLoginInfo user.UserInfo, userAccessedInfo user.UserInfo) bool {
+			if userLoginInfo.Role == user.RoleApprover &&
+				userLoginInfo.DepartmentCode == userAccessedInfo.DepartmentCode {
+				return true
+			} else {
+				return false
+			}
+		},
+		Operation:   OPS_CREATE | OPS_RETRIEVE | OPS_UPDATE | OPS_DELETE,
+		Permission:  true,
+		Description: "Allow RoleRoleApprover to do CRUD ops to all users in the same department",
+	})
+
+	TheUserAuthorityTable = append(TheUserAuthorityTable, UserAuthority{
+		RelationShipBetween: func(userLoginInfo user.UserInfo, userAccessedInfo user.UserInfo) bool {
+			if userLoginInfo.Role == user.RoleProjectChief &&
+				userLoginInfo.UserID == userAccessedInfo.UserID {
+				return true
+			} else {
+				return false
+			}
+		},
+		Operation:   OPS_RETRIEVE,
+		Permission:  true,
+		Description: "Allow RoleProjectChief to do retrieve his own info",
+	})
 }
 
 func UserAuthorityCheck(userLoginID, userAccessedID int, ops int) (userLoginInfo user.UserInfo, userAccessedInfo user.UserInfo,
@@ -56,7 +84,7 @@ func UserAuthorityCheck(userLoginID, userAccessedID int, ops int) (userLoginInfo
 	}
 
 	for _, rule := range TheUserAuthorityTable {
-		if ops&rule.Operation != 0 && rule.RelationShipBetween(userLoginInfo, userAccessedInfo) == true {
+		if (ops&rule.Operation != 0) && rule.RelationShipBetween(userLoginInfo, userAccessedInfo) == true {
 			// There are two priority options: allow priority, do not allow priority
 			// We choose do allow priority
 			if rule.Permission == true {
@@ -72,5 +100,9 @@ func UserAuthorityCheck(userLoginID, userAccessedID int, ops int) (userLoginInfo
 
 	// There are two default options here: default allowed, default not allowed
 	// We choose default not allowed, since we choose do allow priority
+	logMap.Log(logMap.NORMAL).WithFields(logrus.Fields{
+		"UserLoginInfo":    userLoginInfo,
+		"UserAccessedInfo": userAccessedInfo,
+	}).Info("no permission allow matched")
 	return userLoginInfo, userAccessedInfo, false, nil
 }
