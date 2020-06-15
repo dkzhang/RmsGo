bc0fa6692df8cda433cfa95d7add12cd040984c9
dkzhang@Office.PC <527656264@qq.com>
2020-06-15 10:15:32 +0800

    update Application

--- webapi/handleProRes/projectAndResource.go
+++ webapi/handleProRes/projectAndResource.go
@@ -1,14 +1,58 @@
 package handleProRes
 
-import "time"
+import (
+	"github.com/gin-gonic/gin"
+	"time"
+)
 
+type NewProResWA struct {
+	ProjectID     int
+	ApplicationID int
+	ProjectName   string
+	Resource      ResourceApplication
+	Action        int
+	ExtraInfo     string
+}
+
+func ApplyNewProRes(c *gin.Context) {
+
+}
+
+// Set up a JSON-based Application struct in a unified format
+// to facilitate the generation of historical record arrays
 type Application struct {
 	ProjectID     int
 	ApplicationID int
-	Action        int
-	BasicInfo     string
-	ExtraInfo     string
+	Type          int
+	Status        int
+	BasicContent  string
+	ExtraContent  string
+	TheOpsRecords []AppOpsRecord
 }
+
+type AppOpsRecord struct {
+	OpsUserID          int
+	OpsUserChineseName int
+	Action             int
+	OpsDatetime        time.Time
+	BasicInfo          string
+	ExtraInfo          string
+}
+
+const (
+	AppStatusProjectChief = 1
+	AppStatusApprover     = 2
+	AppStatusController   = 7
+	AppStatusArchived     = 9
+)
+
+const (
+	AppTypeNew           = 1
+	AppTypeExpansion     = 2
+	AppTypeReturnCompute = 3
+	AppTypeReturnStorage = 4
+	AppTypeMetering      = 5
+)
 
 type ProjectInfo struct {
 	ID   int
