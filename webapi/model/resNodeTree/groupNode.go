251e01cc4672e64406aa41c33f289ef7f4884810
dkzhang@Office.PC <527656264@qq.com>
2020-08-28 14:27:04 +0800

    accomplish res Node Tree toJson & from Json.

--- webapi/model/resNodeTree/groupNode.go
+++ webapi/model/resNodeTree/groupNode.go
@@ -1,22 +1,77 @@
 package resNodeTree
 
+import (
+	"encoding/json"
+	"fmt"
+)
+
 type Group struct {
-	ID          int     `json:"group_id"`
-	Name        string  `json:"group_name"`
-	Status      int     `json:"group_status"`
-	Description string  `json:"description"`
-	SubGroups   []Group `json:"sub_groups"`
-	Nodes       []Node  `json:"nodes"`
+	ID             int         `json:"group_id"`
+	Name           string      `json:"group_name"`
+	Status         int         `json:"group_status"`
+	Description    string      `json:"description"`
+	SubGroups      []*Group    `json:"sub_groups"`
+	Nodes          []*Node     `json:"nodes"`
+	NodesNum       int         `json:"nodes_num"`
+	NodesStatusMap map[int]int `json:"nodes_status_map"`
 }
 
 type Node struct {
-	ID          int    `json:"node_id"`
-	Name        string `json:"node_name"`
-	Status      int    `json:"node_status"`
-	Description string `json:"description"`
+	ID          int    `db:"node_id" json:"node_id"`
+	Name        string `db:"node_name" json:"node_name"`
+	Status      int    `db:"node_status" json:"node_status"`
+	Description string `db:"description" json:"description"`
+	ProjectID   int    `db:"project_id" json:"project_id"`
 }
 
-func Count(g Group) (nodesNum int, nodesStatusMap map[int]int) {
+///////////////////////////////////////////////////////////////////////////////
+
+const (
+	StatusUnselected        = 1
+	StatusPartiallySelected = 2
+	StatusSelected          = 4
+)
+
+///////////////////////////////////////////////////////////////////////////////
+
+func CountGroup(g *Group) {
+	if g == nil {
+		return
+	}
+
+	g.NodesStatusMap = make(map[int]int)
+	g.NodesNum = 0
+
+	if g.Nodes != nil {
+		for _, node := range g.Nodes {
+			g.NodesNum++
+			g.NodesStatusMap[node.Status]++
+		}
+	}
+
+	if g.SubGroups != nil {
+		for _, sg := range g.SubGroups {
+			CountGroup(sg)
+			g.NodesNum += sg.NodesNum
+			for k, v := range sg.NodesStatusMap {
+				g.NodesStatusMap[k] += v
+			}
+		}
+	}
+
+	if g.NodesStatusMap[StatusSelected]+g.NodesStatusMap[StatusUnselected] != 0 {
+		if g.NodesStatusMap[StatusUnselected] == 0 {
+			g.Status = StatusSelected
+		} else if g.NodesStatusMap[StatusSelected] == 0 {
+			g.Status = StatusUnselected
+		} else {
+			g.Status = StatusPartiallySelected
+		}
+	}
+	return
+}
+
+func CountGroupRO(g Group) (nodesNum int, nodesStatusMap map[int]int) {
 	nodesStatusMap = make(map[int]int)
 	nodesNum = 0
 
@@ -29,7 +84,7 @@ func Count(g Group) (nodesNum int, nodesStatusMap map[int]int) {
 
 	if g.SubGroups != nil {
 		for _, sg := range g.SubGroups {
-			num, sMap := Count(sg)
+			num, sMap := CountGroupRO(*sg)
 			nodesNum += num
 			for k, v := range sMap {
 				nodesStatusMap[k] += v
@@ -39,22 +94,26 @@ func Count(g Group) (nodesNum int, nodesStatusMap map[int]int) {
 	return nodesNum, nodesStatusMap
 }
 
-func OrganizeGroupStatus(g Group) (status int) {
-	num, sMap := Count(g)
-	if num != 0 {
-		if sMap[StatusUnselected] == 0 {
-			status = StatusSelected
-		} else if sMap[StatusSelected] == 0 {
-			status = StatusUnselected
-		} else {
-			status = StatusPartiallySelected
-		}
+func GroupToJson(g Group) (string, error) {
+	bj, err := json.Marshal(g)
+	if err != nil {
+		return "", fmt.Errorf("json Marshal Group error: %v", err)
 	}
-	return status
+	return string(bj), nil
 }
 
-const (
-	StatusUnselected        = 1
-	StatusPartiallySelected = 2
-	StatusSelected          = 4
-)
+func GroupToJsonIndent(g Group) (string, error) {
+	bj, err := json.MarshalIndent(g, "", "    ")
+	if err != nil {
+		return "", fmt.Errorf("json Marshal Group error: %v", err)
+	}
+	return string(bj), nil
+}
+
+func LoadGroupFromJson(str string) (g Group, err error) {
+	err = json.Unmarshal([]byte(str), &g)
+	if err != nil {
+		return Group{}, fmt.Errorf("json UnMarshal Group error: %v", err)
+	}
+	return g, nil
+}
--- webapi/model/resNodeTree/resNodeTree.go
+++ webapi/model/resNodeTree/resNodeTree.go
@@ -1,5 +1,55 @@
 package resNodeTree
 
+import (
+	"fmt"
+)
+
 type Tree struct {
 	RootGroup Group
+	NodesMap  map[int]*Node
+}
+
+func LoadTreeFromJson(str string) (t Tree, err error) {
+	t.RootGroup, err = LoadGroupFromJson(str)
+	if err != nil {
+		return Tree{}, fmt.Errorf("LoadFromJson error since LoadGroupFromJson error: %v", err)
+	}
+
+	//GenNodesMap
+	t.NodesMap = make(map[int]*Node)
+	t.NodesMap, err = genNodesMap(&(t.RootGroup), t.NodesMap)
+	if err != nil {
+		return Tree{}, fmt.Errorf("tree genNodesMap error: %v", err)
+	}
+	return t, nil
+}
+
+func genNodesMap(g *Group, nm map[int]*Node) (map[int]*Node, error) {
+	var err error
+	if g.Nodes != nil {
+		for _, node := range g.Nodes {
+			if _, ok := nm[node.ID]; ok {
+				return nil, fmt.Errorf(" Group<%d> error: duplicated ID <%d> detected", g.ID, node.ID)
+			}
+			nm[node.ID] = node
+		}
+	}
+
+	if g.SubGroups != nil {
+		for _, sg := range g.SubGroups {
+			nm, err = genNodesMap(sg, nm)
+			if err != nil {
+				return nil, fmt.Errorf(" Group<%d>-subGroup<<%d>> error: %v", g.ID, sg.ID, err)
+			}
+		}
+	}
+	return nm, nil
+}
+
+func TreeToJson(t Tree) (string, error) {
+	return GroupToJson(t.RootGroup)
+}
+
+func TreeToJsonIndent(t Tree) (string, error) {
+	return GroupToJsonIndent(t.RootGroup)
 }
