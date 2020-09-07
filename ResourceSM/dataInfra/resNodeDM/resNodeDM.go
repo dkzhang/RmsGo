35a8aaf5bf9141e9f14ebb8c763f3c072d972f25
dkzhang@Office.PC <527656264@qq.com>
2020-09-07 16:28:21 +0800

    add projectRes for project allocate info

--- ResourceSM/dataInfra/resNodeDM/resNodeDM.go
+++ ResourceSM/dataInfra/resNodeDM/resNodeDM.go
@@ -1,1 +1,10 @@
 package resNodeDM
+
+import "github.com/dkzhang/RmsGo/ResourceSM/model/resNode"
+
+type ResNodeDM interface {
+	QueryByID(nodeID int) (resNode.Node, error)
+	QueryAll() ([]resNode.Node, error)
+	Update(node resNode.Node) (err error)
+	Insert(node resNode.Node) (err error)
+}
