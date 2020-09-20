ef66dca5067307af06118d6901cf142d0cafc494
dkzhang <dk@home.pc>
2020-09-20 19:44:13 +0800

    add compute after from before & change

--- myUtils/arrayMerge/computeChange.go
+++ myUtils/arrayMerge/computeChange.go
@@ -81,20 +81,94 @@ func ComputeChange(before []int64, after []int64) (change []int64, increased int
 	return change, increased, reduced, nil
 }
 
-//func ApplyBeforeChange(before []int64, change []int64)(after []int64, err error){
-//	after := make([]int, 0)
-//
-//	sort.Sort(Int64Slice(before))
-//	sort.Sort(AbsInt64Slice(change))
-//
-//
-//	for i,j:=0,0;i<len(before)&&j<len(change);{
-//		if before[i] < AbsInt64(change[j]){
-//
-//		}else if before[i] == AbsInt64(change[j]){
-//
-//		}else{
-//
-//		}
-//	}
-//}
+func ApplyBeforeChange(before []int64, change []int64) (after []int64, err error) {
+	if change == nil || len(change) == 0 {
+		// no change
+		if before == nil {
+			return []int64{}, nil
+		} else {
+			return before, nil
+		}
+	}
+
+	after = make([]int64, 0)
+	sort.Sort(AbsInt64Slice(change))
+
+	if before == nil || len(before) == 0 {
+		for j, c := range change {
+			if c > 0 {
+				after = append(after, c)
+			} else {
+				return nil,
+					fmt.Errorf("negative value change[%d]=%d is illegal since before array is empty", j, c)
+			}
+		}
+		return after, nil
+	}
+
+	sort.Sort(Int64Slice(before))
+
+	// positive&zero check for before array
+	if before[0] <= 0 {
+		return nil,
+			fmt.Errorf("negative/zero value %d in before array is illegal", before[0])
+	}
+	if change[0] == 0 {
+		return nil,
+			fmt.Errorf("zero value %d in change array is illegal", change[0])
+	}
+
+	// two index scan
+	i, j := 0, 0
+	for i < len(before) && j < len(change) {
+		if before[i] < AbsInt64(change[j]) {
+			after = append(after, before[i])
+			i++
+		} else if before[i] == AbsInt64(change[j]) {
+			if change[j] < 0 {
+				i++
+				j++
+				continue
+			} else {
+				return nil,
+					fmt.Errorf("positive value change[%d]=%d is illegal since it is already existed in the before array, change=%v", j, change[j], change)
+			}
+		} else {
+			if change[j] > 0 {
+				after = append(after, change[j])
+				j++
+			} else {
+				return nil,
+					fmt.Errorf("negative value change[%d]=%d is illegal since it is not exist in the before array", j, change[j])
+			}
+		}
+	}
+	if i == len(before) {
+		for jt := j; jt < len(change); jt++ {
+			if change[jt] > 0 {
+				after = append(after, change[jt])
+			} else {
+				return nil,
+					fmt.Errorf("negative value change[%d]=%d is illegal since before array is empty", jt, change[jt])
+			}
+		}
+	} else {
+		for it := i; it < len(before); it++ {
+			after = append(after, before[it])
+		}
+	}
+
+	return after, nil
+}
+
+func ApplyBeforeSubtract(before []int64, subtract []int64) (after []int64, err error) {
+	change := make([]int64, len(subtract))
+	for i, s := range subtract {
+		if s <= 0 {
+			return nil, fmt.Errorf("subtract positive check failed: subtract[%d]=%d", i, s)
+		}
+		change[i] = -s
+	}
+
+	return ApplyBeforeChange(before, change)
+}
--- myUtils/arrayMerge/computeChange_test.go
+++ myUtils/arrayMerge/computeChange_test.go
@@ -7,7 +7,7 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("ComputeChange", func() {
+var _ = Describe("Array Merge", func() {
 	Describe("ComputeChange", func() {
 		It("normal", func() {
 			before := []int64{1, 2, 5, 8, 9, 3}
@@ -82,6 +82,141 @@ var _ = Describe("ComputeChange", func() {
 
 			Expect(err).ShouldNot(HaveOccurred())
 			By(fmt.Sprintf("change=%v, increased=%d, reduced=%d, err=%v", change, increased, reduced, err))
+		})
+	})
+
+	Describe("ApplyBeforeChange", func() {
+		It("normal", func() {
+			var before, change, after []int64
+			var err error
+
+			before = []int64{2, 4, 6, 8, 10, 12, 14, 16}
+			change = []int64{1, 3, -2, -16}
+			after, err = arrayMerge.ApplyBeforeChange(before, change)
+
+			Expect(err).ShouldNot(HaveOccurred())
+			By(fmt.Sprintf("before=%v, change=%v, after=%v, err=%v", before, change, after, err))
+
+			before = []int64{2, 4, 6, 8, 10, 12, 14, 16}
+			change = []int64{-12, -16}
+			after, err = arrayMerge.ApplyBeforeChange(before, change)
+
+			Expect(err).ShouldNot(HaveOccurred())
+			By(fmt.Sprintf("before=%v, change=%v, after=%v, err=%v", before, change, after, err))
+		})
+
+		It("before is nil normal", func() {
+			var before, change []int64
+			before = nil
+			change = []int64{12, 16}
+			after, err := arrayMerge.ApplyBeforeChange(before, change)
+
+			Expect(err).ShouldNot(HaveOccurred())
+			By(fmt.Sprintf("before=%v, change=%v, after=%v, err=%v", before, change, after, err))
+		})
+
+		It("before is nil error", func() {
+			var before, change []int64
+			before = nil
+			change = []int64{-12, -16}
+			after, err := arrayMerge.ApplyBeforeChange(before, change)
+
+			Expect(err).Should(HaveOccurred())
+			By(fmt.Sprintf("before=%v, change=%v, after=%v, err=%v", before, change, after, err))
+		})
+
+		It("change is nil normal", func() {
+			var before, change []int64
+			before = []int64{2, 4, 6, 8, 10, 12, 14, 16}
+			change = nil
+			after, err := arrayMerge.ApplyBeforeChange(before, change)
+
+			Expect(err).ShouldNot(HaveOccurred())
+			By(fmt.Sprintf("before=%v, change=%v, after=%v, err=%v", before, change, after, err))
+		})
+
+		It("before & change is nil ", func() {
+			var before, change []int64
+			before = nil
+			change = nil
+			after, err := arrayMerge.ApplyBeforeChange(before, change)
+
+			Expect(err).ShouldNot(HaveOccurred())
+			By(fmt.Sprintf("before=%v, change=%v, after=%v, err=%v", before, change, after, err))
+		})
+
+		It("duplicate value ", func() {
+			var before, change []int64
+			before = []int64{2, 4, 6, 8, 10, 12, 14, 16}
+			change = []int64{1, 3, -2, -16, 10}
+			after, err := arrayMerge.ApplyBeforeChange(before, change)
+
+			Expect(err).Should(HaveOccurred())
+			By(fmt.Sprintf("before=%v, change=%v, after=%v, err=%v", before, change, after, err))
+		})
+
+		It("zero value ", func() {
+			var before, change, after []int64
+			var err error
+
+			before = []int64{2, 4, 6, 8, 10, 12, 14, 16}
+			change = []int64{1, 3, -2, -16, 0}
+			after, err = arrayMerge.ApplyBeforeChange(before, change)
+
+			Expect(err).Should(HaveOccurred())
+			By(fmt.Sprintf("before=%v, change=%v, after=%v, err=%v", before, change, after, err))
+
+			before = []int64{2, 4, 6, 8, 0, 10, 12, 14, 16}
+			change = []int64{1, 3, -2, -16}
+			after, err = arrayMerge.ApplyBeforeChange(before, change)
+
+			Expect(err).Should(HaveOccurred())
+			By(fmt.Sprintf("before=%v, change=%v, after=%v, err=%v", before, change, after, err))
+		})
+	})
+
+	Describe("ApplyBeforeSubtract", func() {
+		It("normal", func() {
+			var before, subtract, after []int64
+			var err error
+
+			before = []int64{2, 4, 6, 8, 10, 12, 14, 16}
+			subtract = []int64{14, 8, 4, 6}
+			after, err = arrayMerge.ApplyBeforeSubtract(before, subtract)
+
+			Expect(err).ShouldNot(HaveOccurred())
+			By(fmt.Sprintf("before=%v, subtract=%v, after=%v, err=%v", before, subtract, after, err))
+
+			before = []int64{2, 4, 6, 8, 10, 12, 14, 16}
+			subtract = []int64{16, 4}
+			after, err = arrayMerge.ApplyBeforeSubtract(before, subtract)
+
+			Expect(err).ShouldNot(HaveOccurred())
+			By(fmt.Sprintf("before=%v, subtract=%v, after=%v, err=%v", before, subtract, after, err))
+		})
+
+		It("negative value in subtract", func() {
+			var before, subtract, after []int64
+			var err error
+
+			before = []int64{2, 4, 6, 8, 10, 12, 14, 16}
+			subtract = []int64{14, 8, 2, -10, 6}
+			after, err = arrayMerge.ApplyBeforeSubtract(before, subtract)
+
+			Expect(err).Should(HaveOccurred())
+			By(fmt.Sprintf("before=%v, subtract=%v, after=%v, err=%v", before, subtract, after, err))
+		})
+
+		It("subtract value not exist in before", func() {
+			var before, subtract, after []int64
+			var err error
+
+			before = []int64{2, 4, 6, 8, 10, 12, 14, 16}
+			subtract = []int64{14, 8, 2, 11, 6}
+			after, err = arrayMerge.ApplyBeforeSubtract(before, subtract)
+
+			Expect(err).Should(HaveOccurred())
+			By(fmt.Sprintf("before=%v, subtract=%v, after=%v, err=%v", before, subtract, after, err))
 		})
 	})
 })
