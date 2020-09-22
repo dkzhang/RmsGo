ee37b218f89b99d12a17235a9ce1c22bcdeab03d
dkzhang@Office.PC <527656264@qq.com>
2020-09-22 16:04:30 +0800

    accomplish meteringClient

--- webapi/SmsNotifier/smsNotifier.go
+++ webapi/SmsNotifier/smsNotifier.go
@@ -7,12 +7,47 @@ import (
 	"github.com/dkzhang/RmsGo/webapi/dataInfra/userDM"
 	"github.com/dkzhang/RmsGo/webapi/model/user"
 	"github.com/sirupsen/logrus"
+	"time"
 )
 
 type Notifier struct {
 	theUserDM     userDM.UserDM
 	TheSmsService shortMessageService.SmsService
 	TheLogMap     logMap.LogMap
+}
+
+func (nt Notifier) Notify(ids []int, msg []string) (err error) {
+	for _, userID := range ids {
+		userInfo, err := nt.theUserDM.QueryUserByID(userID)
+		if err != nil {
+			return fmt.Errorf("theUserDM.QueryUserByID query userID <%d> error: %v", userID, err)
+		}
+
+		msg[0] = userInfo.ChineseName
+
+		for retryTimes := 0; retryTimes < 3; retryTimes++ {
+			resp, err := nt.TheSmsService.SendSMS(shortMessageService.MessageContent{
+				PhoneNumberSet:   []string{shortMessageService.ChineseMobile(userInfo.Mobile)},
+				TemplateParamSet: msg,
+				TemplateType:     shortMessageService.TemplatePwd,
+			})
+
+			nt.TheLogMap.Log(logMap.NORMAL).WithFields(logrus.Fields{
+				"UserID": userInfo.UserID,
+				"Mobile": userInfo.Mobile,
+				"msg":    msg,
+				"resp":   resp,
+				"err":    err,
+			}).Info("NotifyProjectChief success.")
+
+			if err == nil {
+				break
+			}
+
+			time.Sleep(time.Second * 10)
+		}
+	}
+	return nil
 }
 
 func (nt Notifier) NotifyProjectChief(userID int, msg string) (err error) {
