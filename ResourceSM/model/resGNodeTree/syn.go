8301bb968d7ad903be5c56c50c79c863bc89dfc3
dkzhang@Office.PC <527656264@qq.com>
2020-09-24 14:48:33 +0800

    update syn tree func.

--- ResourceSM/model/resGNodeTree/syn.go
+++ ResourceSM/model/resGNodeTree/syn.go
@@ -4,57 +4,81 @@ import (
 	"fmt"
 	"github.com/dkzhang/RmsGo/ResourceSM/model/resGNode"
 	"github.com/dkzhang/RmsGo/ResourceSM/model/resNode"
-	"github.com/golang-collections/collections/stack"
 )
 
-// syn the nodes info from DB to the Tree struct
-func SynchronizeNodesInfo(t *Tree, nodesMap map[int64]resNode.Node) (err error) {
-
-	giStack := stack.New()
-
-	giStack.Push(gNodeAndIndex{
-		gNode: &t.Root,
-		index: 0,
-	})
-
-	for {
-		if giStack.Len() == 0 {
-			break
+func synNodesInfo(node *resGNode.ResGNode, nodesMap map[int64]resNode.Node) (err error) {
+	if node.ID < resGNode.GroupBase {
+		_, ok := nodesMap[node.ID]
+		if !ok {
+			return fmt.Errorf("GNode info (id=%d) does not exist in the NodesMap", node.ID)
+		} else {
+			node.Label = nodesMap[node.ID].Name
 		}
+	}
 
-		gi, _ := giStack.Pop().(gNodeAndIndex)
-
-		if gi.gNode.Children != nil {
-			if gi.index < len(gi.gNode.Children) {
-				// push current group
-				giStack.Push(gNodeAndIndex{
-					gNode: gi.gNode,
-					index: gi.index + 1,
-				})
-
-				// push a sub group
-				giStack.Push(gNodeAndIndex{
-					gNode: gi.gNode.Children[gi.index],
-					index: 0,
-				})
-			} else {
-				//At the end of Children
-
-				// scan all current gNode children
-				// set all node children label = name from db
-
-				for _, child := range gi.gNode.Children {
-					if child.ID < resGNode.GroupBase {
-						_, ok := nodesMap[child.ID]
-						if !ok {
-							return fmt.Errorf("GNode info (id=%d) does not exist in the NodesMap", child.ID)
-						} else {
-							child.Label = nodesMap[child.ID].Name
-						}
-					}
-				}
+	if node.Children != nil {
+		for i, _ := range node.Children {
+			err = synNodesInfo(node.Children[i], nodesMap)
+			if err != nil {
+				return err
 			}
 		}
 	}
 	return nil
 }
+
+func SynchronizeNodesInfo(t *Tree, nodesMap map[int64]resNode.Node) (err error) {
+	return synNodesInfo(&t.Root, nodesMap)
+}
+
+// syn the nodes info from DB to the Tree struct
+//func SynchronizeNodesInfo(t *Tree, nodesMap map[int64]resNode.Node) (err error) {
+//
+//	giStack := stack.New()
+//
+//	giStack.Push(gNodeAndIndex{
+//		gNode: &t.Root,
+//		index: 0,
+//	})
+//
+//	for {
+//		if giStack.Len() == 0 {
+//			break
+//		}
+//
+//		gi, _ := giStack.Pop().(gNodeAndIndex)
+//
+//		if gi.gNode.Children != nil {
+//			if gi.index < len(gi.gNode.Children) {
+//				// push current group
+//				giStack.Push(gNodeAndIndex{
+//					gNode: gi.gNode,
+//					index: gi.index + 1,
+//				})
+//
+//				// push a sub group
+//				giStack.Push(gNodeAndIndex{
+//					gNode: gi.gNode.Children[gi.index],
+//					index: 0,
+//				})
+//			} else {
+//				//At the end of Children
+//
+//				// scan all current gNode children
+//				// set all node children label = name from db
+//
+//				for _, child := range gi.gNode.Children {
+//					if child.ID < resGNode.GroupBase {
+//						_, ok := nodesMap[child.ID]
+//						if !ok {
+//							return fmt.Errorf("GNode info (id=%d) does not exist in the NodesMap", child.ID)
+//						} else {
+//							child.Label = nodesMap[child.ID].Name
+//						}
+//					}
+//				}
+//			}
+//		}
+//	}
+//	return nil
+//}
