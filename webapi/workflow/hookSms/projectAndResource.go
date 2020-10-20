e1149bf7f2d98000eba53592357d8824e40f05f2
dkzhang@Office.PC <527656264@qq.com>
2020-10-20 11:43:20 +0800

    add part of hook for workflow.

---
+++ webapi/workflow/hookSms/projectAndResource.go
@@ -0,0 +1,20 @@
+package hookSms
+
+import (
+	"github.com/dkzhang/RmsGo/myUtils/webapiError"
+	"github.com/dkzhang/RmsGo/webapi/dataInfra/userDM"
+	"github.com/dkzhang/RmsGo/webapi/model/generalForm"
+	"github.com/dkzhang/RmsGo/webapi/model/user"
+)
+
+func HookPR(udm userDM.UserDM) {
+
+}
+
+func Submit(form generalForm.GeneralForm, userInfo user.UserInfo, appID int, waErr webapiError.Err) {
+
+}
+
+func ReSubmit(form generalForm.GeneralForm, userInfo user.UserInfo, appID int, waErr webapiError.Err) {
+
+}
